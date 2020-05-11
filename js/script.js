3ca1e22d7264679ef3177c451f932c546cc96bfa
Joyce Kelly <63926941+Joycekellyy@users.noreply.github.com>
2020-05-11 14:13:19 -0300

    Script

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var resposta = window.document.getElementById('resposta')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var txtano = window.document.getElementById('txtano') 
+    if (txtano.value.length == 0 || Number(txtano.value) > ano) {
+        window.alert('[ERRO] Insira os dados corretamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var nasc = Number(txtano.value)
+        var idade = ano - nasc
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade <=10) {
+                //criança
+                img.setAttribute('src', 'fotos/crianca-f.png')
+            }else if (idade < 21) {
+                //jpvem
+                img.setAttribute('src', 'fotos/jovem-f.png')
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'fotos/adulto-f.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'fotos/idoso-f.png')
+            }
+        }else {
+            gênero = 'Homem'
+            if (idade >=0 && idade <=10) {
+                //criança
+                img.setAttribute('src', 'fotos/crianca-m.png')
+            }else if (idade < 21) {
+                //jpvem
+                img.setAttribute('src', 'fotos/jovem-m.png')
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'fotos/adulto-m.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'fotos/idoso-m.png')
+            }
+        }
+        resposta.style.textAlign = 'center'
+        resposta.innerHTML = `<p>Detectamos um(a) ${gênero} com ${idade} anos de idade.</p>`
+        resposta.appendChild(img)
+    }
+}
